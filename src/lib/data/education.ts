03b75950eb7bec5c2c6f73a20518981ea8e7934a
Chase Rushton <chaserushton@gmail.com>
2024-06-16 23:07:26 -0400

    Update education.ts

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,13 +3,13 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
+		degree: 'B.S Molecular and Cellular Microbiology ',
 		description: '',
-		location: 'Tunisia',
+		location: 'USA',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		organization: 'Auburn university ',
+		period: { from: new Date(2011), to: new Date(2015) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
 		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
